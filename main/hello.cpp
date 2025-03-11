e75222a0cb7457d8a26c85b256eb4385d440633f
Manav MN <manav.nayak2004@gmail.com>
2025-03-11 23:01:51 +0530

    Added changed hello again

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, Manav\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Nayak\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
